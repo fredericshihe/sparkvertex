ec7a707d57f666a103b256f63f982cc2fc59f5e4
SHIHE <shihe@SHIHEdeMacBook-Pro.local>
2025-11-26 12:59:09 +0800

    Clean next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // ... other config
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
-
-/*
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-});
-
-module.exports = withPWA(nextConfig);
-*/
